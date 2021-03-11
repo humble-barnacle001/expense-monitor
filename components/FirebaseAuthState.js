a8b997c096cfa80f1ec43936fc92556cff6afb06
humble-barnacle001 <barnaclenshade@protonmail.com>
2021-03-11 13:09:45 +0530

    Remove firebase admin

--- components/FirebaseAuthState.js
+++ components/FirebaseAuthState.js
@@ -24,41 +24,10 @@ export default function FirebaseAuthState({ children }) {
                         type: "LOGOUT"
                     });
                 } else {
-                    const { token } = await user.getIdTokenResult(true);
-                    fetch(`/api/current-user`, {
-                        headers: {
-                            Authorization: `Bearer ${token}`
-                        }
-                    })
-                        .then((r) =>
-                            r.json().then((res) => {
-                                if (res.status === 200) {
-                                    dispatch({
-                                        type: "LOGIN",
-                                        payload: res.body.user
-                                    });
-                                } else
-                                    throw new Error(
-                                        res.body.errors
-                                            ? res.body.errors
-                                                  .map((err) => err.msg)
-                                                  .toString()
-                                            : "Unable to login user"
-                                    );
-                            })
-                        )
-                        .catch((err) => {
-                            console.log("Error in fetch: ", err.message);
-                            dispatch({
-                                type: "LOGOUT"
-                            });
-                            window.halfmoon.initStickyAlert({
-                                content: "Verification failed!!",
-                                title: "Error!!",
-                                alertType: "alert-danger",
-                                fillType: "filled"
-                            });
-                        });
+                    dispatch({
+                        type: "LOGIN",
+                        payload: { uid: user.uid }
+                    });
                 }
             },
             (e) => {
